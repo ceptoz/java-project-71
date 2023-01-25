05ce9d911d5d40552c0c7ce75b78005641b3a646
ceptoz <brylin.alex@gmail.com>
2023-01-25 14:46:22 +0400

    Small remake of picocle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,15 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
+import java.util.concurrent.Callable;
+
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable{
+public class App implements Callable<Integer> {
     @Override
-    public void run() {
+    public Integer call() {
         System.out.println("Hello!");
+        return 0;
     }
 
     public static void main(String[] args) {

51979ef087996ece821a7d05a0f7ef9748395561
ceptoz <brylin.alex@gmail.com>
2023-01-25 14:44:32 +0400

    Set running with picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,17 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Hello!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }

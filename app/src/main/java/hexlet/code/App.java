43f0a28744a2ebd75e5dba9e2b45d086d386703d
ceptoz <brylin.alex@gmail.com>
2023-01-25 16:52:14 +0400

    Added options within picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,11 +2,19 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format", defaultValue = "stylish", showDefaultValue = CommandLine.Help.Visibility.ALWAYS, paramLabel = "format")
+    private String format = "stylish";
+    @CommandLine.Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private File filepath1;
+    @CommandLine.Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private File filepath2;
+
     @Override
     public Integer call() {
         System.out.println("Hello!");
